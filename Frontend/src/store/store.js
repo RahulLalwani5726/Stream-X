51752452386c50d775972adddfce18ab03ddcfc5
Rahul Lalwani 57226 <156502677+RahulLalwani5726@users.noreply.github.com>
2026-01-17 09:00:11 +0530

    Delete Frontend/src directory

--- Frontend/src/store/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import authSlice from './feature/auth'
-
-const store = configureStore({
-    reducer: { auth: authSlice.reducer }
-})
-
-export default store
